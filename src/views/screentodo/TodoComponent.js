15a98edc0c82b1fadadb330f4760ffe6fb3a4bab
Hùng Tăng <minhhung290701@gmail.com>
2022-06-29 17:09:00 +0700

    add first new component

---
+++ src/views/screentodo/TodoComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Todocomponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                aaa
+            </div>
+        )
+    }
+}
+
+export default Todocomponent;
